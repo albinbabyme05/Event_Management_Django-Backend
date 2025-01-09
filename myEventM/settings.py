d73b54a537d8cf637f87bed2f346cc4a840b6c55
Albin Baby <albinbabayme05@gmail.com>
2025-01-09 15:04:12 +0100

    HTML page created

--- myEventM/settings.py
+++ myEventM/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'eventapp'
+    'eventapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
