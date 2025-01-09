160832c323778b9da0ce88babaf5e44a2ec979a0
Albin Baby <albinbabayme05@gmail.com>
2025-01-09 22:52:09 +0100

    retruned dictionary

--- eventapp/forms.py
+++ eventapp/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from . models import Booking
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+    
 #creating a duplicate model for user to enter details
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
+        
+        #add date picker
+        widgets = {
+            'bookingDate' : DateInput(),
+        }
+
+        #to change the labels
+        labels = {
+             'customerName': "Customer Name:",
+             'customerPhone' : "Customer Phone:",
+             'name' : "Event Name:",
+              'bookingDate' : "Booking Date:",
+        }
