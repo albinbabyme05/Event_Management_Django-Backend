6a0c01380671394983304c60d4ca92af00981aec
Albin Baby <albinbabayme05@gmail.com>
2025-01-09 22:21:22 +0100

    name of the event will show in the adim panel and dict created for fetching duplicate model form fields

--- eventapp/models.py
+++ eventapp/models.py
@@ -6,6 +6,10 @@ class Event(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     
+    #to show the name in the database column as string
+    def __str__(self):
+        return self.name
+    
 class Booking(models.Model):
     customerName = models.CharField(max_length=50)
     customerPhone = models.CharField(max_length=12)
